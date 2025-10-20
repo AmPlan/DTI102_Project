a0eea12a7efe93d39135cc29693416557c5eebae
AmPlan <116545595+AmPlan@users.noreply.github.com>
2025-10-20 09:47:21 +0700

    Initial

---
+++ test.py
@@ -0,0 +1,28 @@
+import pygame
+
+# Set up
+pygame.init()
+screen  = pygame.display.set_mode((1280, 720))
+clock   = pygame.time.Clock()
+running = True
+
+# Game loop
+while running:
+
+    # player's action on window
+    for event in pygame.event.get():
+        # user clicked X to close window
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    # update the whole screen
+    pygame.display.flip()
+
+    # Limit the frame rate to 60
+    clock.tick(60)
+
+# Stop the game
+pygame.quit()
