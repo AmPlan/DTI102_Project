d6daed2c4e22626b77af2acd61cc82a524d92f72
AmPlan <116545595+AmPlan@users.noreply.github.com>
2025-10-22 12:28:06 +0700

    Add hello world to screen

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 import pygame
 
+SCREEN_WIDTH  = 1280
+SCREEN_HEIGHT = 720
+
 # Set up
 pygame.init()
-screen  = pygame.display.set_mode((1280, 720))
+screen  = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock   = pygame.time.Clock()
 running = True
+
+
+# Get system font with size of 18
+font = pygame.font.SysFont("Arial", 18) 
 
 # Game loop
 while running:
@@ -16,7 +23,10 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("white")
+    testText = font.render("Hello World!", True, (0, 0, 0))
+
+    screen.blit(testText, (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
 
     # update the whole screen
     pygame.display.flip()
